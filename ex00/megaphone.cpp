deb44da7184ea1a652cf8701f86f7079a1e24545
kaaaaakun <>
2023-12-06 16:46:01 +0900

    fix megaphone

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tokazaki <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tokazaki <tokazaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 19:55:35 by tokazaki          #+#    #+#             */
-/*   Updated: 2023/12/05 18:16:45 by tokazaki         ###   ########.fr       */
+/*   Updated: 2023/12/06 16:44:33 by tokazaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,20 @@
 
 int	main(int argc, char *argv[])
 {
-	int	i;
+	size_t	i;
+	size_t	j;
 
-	if (argc != 2)
-	{
+	if (argc == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (1);
 	}
-	i = 0;
-	while (argv[1][i] != '\0')
-	{
-		std::cout << (char)toupper(argv[1][i]);
+	i = 1;
+	while (argv[i] != NULL) {
+		j = 0;
+		while (argv[i][j] != '\0') {
+			std::cout << (char)toupper(argv[i][j]);
+			j++;
+		}
 		i++;
 	}
 	std::cout << std::endl;
